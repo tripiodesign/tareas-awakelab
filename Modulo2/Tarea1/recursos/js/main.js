797731c14a20699af62fa18f32db1bda2974e451
asbgroup72 <j.geraldo.romeor@hotmail.com>
2020-12-21 20:59:07 -0300

    Reorganizacion de archivos

---
+++ Modulo2/Tarea1/recursos/js/main.js
@@ -0,0 +1,42 @@
+// $('#test').on('click', function() {
+//     $(this).text('Goodbye, Moon!').fadeOut(2000);
+//     $(this).filter('blur:1px;')
+// });
+
+// var car = "Dodge Charger";
+// var someObject = {str:"Algún texto", id:5};
+// console.info("Mi primer carro fue un ", car, ". El objeto es: ", someObject);
+
+// for (var i=0; i<5; i++) {
+//     console.log("Hola, %s. Usted me ha llamado %d veces.", "jonathan", i+2);
+//   }
+
+//condicionales
+
+// var myName = "Jonathan";
+// var myLove = "Christian";
+// var edad = 24;
+
+// var ignasiAge = 32;
+// var ageDiff = 32 - edad;
+
+// console.log("Mi nombre es %s, y tengo %d años.", myName, edad);
+// // console.log("Yo amo a %s con toda mi vida", myLove);
+// console.log("La diferencia de edad es %d", ageDiff);
+// if(edad > 21){
+//     console.log("Tienes mas de 21 años. Eres mayor que Ignacio")
+// }else{
+//     console.log("Tienes menos de 21 añosl. Eres menor que Ignacio")
+// }
+
+
+var names= Array ("Aarón","Abdón","Alvaro","Amadeo","Amaro","Arturo","Aureliano","Aurelio","Baldomero", "Balduino","Blanca","Blas","Bonifacio","Bruno","Calixto","Camilo","Cesáreo","Cipriano","Cirilo");
+
+for (var i = 0; i == 0 & 19 ; i++) {
+    document.write(names[i] + "<br>");
+    // document.write(names.length[19] + "<br>");
+}
+for (var i = 0; i == 19 ; i++) {
+    document.write(names[i] + "<br>");
+    // document.write(names.length[19] + "<br>");
+}
